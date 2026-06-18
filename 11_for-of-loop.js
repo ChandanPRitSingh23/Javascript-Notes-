3f1465dc5d90cdeba9789e5b9d2804db7201d7bd
ChandanPRitSingh23 <chandangithub23@gmail.com>
2026-06-18 20:19:50 +0530

    for of loop

---
+++ 11_for-of-loop.js
@@ -0,0 +1,19 @@
+//? For-of Loop
+//* Syntax
+
+// use for apply loop on the strings and arrays
+
+/*
+for (let val of strVar){
+    do some work
+}
+    */
+
+let str = "Chandan";
+
+let length_string = 0;
+for(let i of str){
+    console.log(i);
+    length_string++;
+}
+console.log(length_string) 
