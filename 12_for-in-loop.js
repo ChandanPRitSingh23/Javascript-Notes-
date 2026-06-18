020f344f06408095bf54eab64bf25cf2ec9b8b9a
ChandanPRitSingh23 <chandangithub23@gmail.com>
2026-06-18 20:24:30 +0530

    done

---
+++ 12_for-in-loop.js
@@ -0,0 +1,21 @@
+//? For in loop
+//* syntax
+ 
+/*
+for(let key in objVar){
+    do some work
+}
+*/
+
+let student = {
+    name : "Chandan",
+    age : 20,
+
+};
+
+for(let i in student){
+    console.log(i);
+}
+
+
+//Ye mainly objects ki properties (keys) ko access karne ke liye use hota hai.
