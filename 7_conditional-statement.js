40340c402f65ed9f85b5b04cb4f198c168ce4274
ChandanPRitSingh23 <chandangithub23@gmail.com>
2026-06-18 17:26:46 +0530

    done

---
+++ 7_conditional-statement.js
@@ -0,0 +1,33 @@
+//? Conditional Statements
+let mode = "dark";
+let color;
+
+if (mode == "dark"){
+    color = "black";
+}
+
+if (mode == "Light"){
+    color = "white";
+}
+
+console.log(color);
+
+//* IF else statement
+let mode = "dark";
+let color;
+if(mode == "dark-mode"){
+    color = "black";
+} else{
+    color = "white";
+} // white
+
+
+//* else if  statement
+let agr = 67;
+if(age<18){
+    console.log("junior");
+} else if(agr>60){
+    console.log("senior");
+}else{
+    console.log("middle");
+} // senior
