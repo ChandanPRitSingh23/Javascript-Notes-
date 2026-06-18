be338609f533b9e16a30dd342c283b6c09aa7564
Chandan Randhawa <chandanrandhawa@Chandans-MacBook-Air.local>
2026-06-18 16:28:21 +0530

    how to comment

---
+++ 6_operators.js
@@ -0,0 +1,37 @@
+//use to perform some operation on data
+
+// + , - , * , /
+
+/*
+ARTIMETIC OPERATORS
+--> modulus  -- %
+--> exponentiation -- power
+--> increment
+--> decrement
+
+*/
+let a =5;
+let b = 2;
+let c = a+b;
+
+console.log("a+b = ",a+b);  //a+b = 7
+console.log("a+b = ",c); //a+b = 7
+
+/*
+
+MODULUS
+a % b = remainder ;
+5 % 2 = 1;
+
+EXPONENTIATION
+a**b = a^b;
+2**2 = 4;
+
+
+
+
+
+
+*/
+
+
