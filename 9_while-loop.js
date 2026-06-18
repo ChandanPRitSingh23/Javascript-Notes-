04f5a5d9a6f0de108b51c5e5c2ece959cafa984c
ChandanPRitSingh23 <chandangithub23@gmail.com>
2026-06-18 19:34:24 +0530

    while loop

---
+++ 9_while-loop.js
@@ -0,0 +1,19 @@
+//? While Loop
+//* Syntax
+
+
+/*
+
+while(condition){
+    do some work
+}
+
+*/
+
+//initialization -> loop ke bhar kro 
+
+let i =1;
+while(i<=5){
+    console.log(i);
+    i++;  //updations
+}
