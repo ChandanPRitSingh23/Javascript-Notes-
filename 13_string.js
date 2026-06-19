c14b3bf05377f4a36c17f98ef4bae5b0e8f0b708
ChandanPRitSingh23 <chandangithub23@gmail.com>
2026-06-19 22:23:47 +0530

    string

--- 13_string.js
+++ 13_string.js
@@ -1,1 +1,11 @@
 //sequence of characters used to represent text 
+let str = "Chandan";
+let str1 = 'Chand';
+console.log(str.length);
+console.log(str[0]);  //that how u find each letter in string
+
+
+//to find the string lenght
+
+//? string_name.length
+
