9ebcae64d5453f0c0a2633ce4bfde6c7bfc6fb1c
ChandanPRitSingh23 <chandangithub23@gmail.com>
2026-06-25 17:04:01 +0530

    question1

---
+++ Day-01/1.js
@@ -0,0 +1,11 @@
+let a = 5;
+let b = 10;
+console.log("Addition: ",a+b);
+
+console.log("Subtraction: ",a-b);
+
+console.log("Multiplication: ",a*b);
+
+console.log("Division: ",a/b);
+
+console.log("Remainder: ",a%b);
