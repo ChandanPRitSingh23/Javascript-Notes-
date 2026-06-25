d91e1dc880b9193309fc7d8b817b19e2c7f0fb78
ChandanPRitSingh23 <chandangithub23@gmail.com>
2026-06-25 19:34:40 +0530

    ques 10

---
+++ Day-01/8.js
@@ -0,0 +1,11 @@
+let year = 2024;
+
+if (year % 400 == 0) {
+    console.log("Leap Year");
+} else if (year % 100 == 0) {
+    console.log("Not a Leap Year");
+} else if (year % 4 == 0) {
+    console.log("Leap Year");
+} else {
+    console.log("Not a Leap Year");
+}
---
+++ Day-01/9.js
@@ -0,0 +1,8 @@
+let age = 20;
+let hasID = true;
+
+if (age >= 18 && hasID) {
+    console.log("Login Allowed");
+} else {
+    console.log("Login Denied");
+}
